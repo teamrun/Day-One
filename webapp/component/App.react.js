d6029c42e80d4183d9acfdab2085fea5d358975c
teamrun <chenllos@163.com>
2015-03-13 20:14:28 +0800

    view切换动画

--- webapp/component/App.react.js
+++ webapp/component/App.react.js
@@ -1,15 +1,22 @@
 var React = require('react');
+var TransitionGroup = require('react/lib/ReactCSSTransitionGroup');
+
 var Router = require('react-router');
 var Link = Router.Link;
 var RouteHandler = Router.RouteHandler;
 
+
+
 var navItems = ['Cal', 'List', 'Photo'];
 
 var App = React.createClass({
+    mixins: [ Router.State ],
     componentDidMount: function(){
         console.timeEnd('Confirmed env -> Load-done spend');
     },
     render: function(){
+        var name = this.getRoutes().reverse()[0].name;
+
         var nodes = navItems.map(function(item){
             return <li><Link to={item.toLowerCase()} >{item}</Link></li>;
         });
@@ -20,13 +27,19 @@ var App = React.createClass({
                         {nodes}
                     </ul>
                 </nav>
-                <main>
-                    <RouteHandler />
-                </main>
+
+                <TransitionGroup component="main" transitionName="viewChange">
+                    <RouteHandler key={name}/>
+                </TransitionGroup>
             </div>
         );
     }
 });
-// var Sample = {};
+
+
+// react-router animation key codes:
+// mixin
+// name
+// TransitionGroup
 
 module.exports = App;
